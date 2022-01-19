de2b8b18897d1464b645c2ca3169effd64ac5ce0
Mario Rosa <mario309060@gmail.com>
2022-01-19 19:55:50 -0300

    sql de criação tabela CATEGORY

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE IF NOT EXISTS eldorado;
 USE eldorado;
+
+CREATE TABLE category (
+         id bigint(20) NOT NULL AUTO_INCREMENT,
+         name varchar(128) not null,
+         PRIMARY KEY (id)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
