8742afe4ac755083f6648673f515778d976d1ace
Mario Rosa <mario309060@gmail.com>
2022-01-19 20:17:49 -0300

    sql de criação tabela DEVICE

--- sql/sql.sql
+++ sql/sql.sql
@@ -7,4 +7,13 @@ CREATE TABLE category (
          PRIMARY KEY (id)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE device (
+         id bigint(20) NOT NULL AUTO_INCREMENT,
+         colorName  varchar(16) not null,
+         partNumber int  not null,
+         categoryId bigint(20) not null,
+         PRIMARY KEY (id)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 
